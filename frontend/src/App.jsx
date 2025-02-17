e0a8ccf669ff01e51204ef6df28c85b78f5aee72
Orkhan Ashrafov <70017511+webofpies@users.noreply.github.com>
2025-02-17 22:50:50 +0400

    chore: mantine portal - reuseTargetNode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { createTheme, MantineProvider } from "@mantine/core";
+import { createTheme, MantineProvider, Portal } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
 import { Notifications } from "@mantine/notifications";
 import { NavigationProgress } from "@mantine/nprogress";
@@ -13,13 +13,20 @@ import "@mantine/charts/styles.css";
 import "mantine-react-table/styles.css";
 import "./index.css";
 import "./highlight.css";
-import routes from "./routes/routes";
 import SoftwareInfo from "./components/Modals/SoftwareInfo";
+import routes from "./routes/routes";
 
 const queryClient = new QueryClient();
 
 const theme = createTheme({
   fontFamily: "Poppins, sans-serif",
+  components: {
+    Portal: Portal.extend({
+      defaultProps: {
+        reuseTargetNode: true,
+      },
+    }),
+  },
 });
 
 function App() {
