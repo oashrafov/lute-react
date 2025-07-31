6ad281a4e9e6f8208599c911007e76a35ec45073
Orkhan Ashrafov <70017511+webofpies@users.noreply.github.com>
2025-07-31 20:59:09 +0400

    install and config typescript

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,12 +4,13 @@ import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import pluginQuery from "@tanstack/eslint-plugin-query";
+import tseslint from "typescript-eslint";
 
-export default [
+export default tseslint.config([
   ...pluginQuery.configs["flat/recommended"],
   { ignores: ["dist"] },
   {
-    files: ["**/*.{js,jsx}"],
+    files: ["**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -27,6 +28,7 @@ export default [
     },
     rules: {
       ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended,
       ...react.configs.recommended.rules,
       ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
@@ -42,4 +44,4 @@ export default [
       ],
     },
   },
-];
+]);
