0fffcda17fc9343eba2f9f135dc1454d16321117
oashrafov <70017511+oashrafov@users.noreply.github.com>
2025-12-07 21:29:27 +0400

    fix: fetch sentence text preview ondemand only

--- frontend/src/features/term/components/TermSentences/SentenceReferenceHoverCard/SentenceReferenceHoverCard.tsx
+++ frontend/src/features/term/components/TermSentences/SentenceReferenceHoverCard/SentenceReferenceHoverCard.tsx
@@ -1,10 +1,11 @@
-import type { ReactNode } from "react";
+import { useState, type ReactNode } from "react";
 import { useSearch } from "@tanstack/react-router";
-import { useQuery } from "@tanstack/react-query";
+import { useQueryClient } from "@tanstack/react-query";
 import { HoverCard, ScrollAreaAutosize } from "@mantine/core";
 import { SentenceTextPreview } from "../SentenceTextPreview/SentenceTextPreview";
 import { query } from "#book/api/query";
 import type { SentenceReferenceData } from "#term/api/types";
+import type { Page } from "#book/api/types";
 
 interface SentenceReferenceHoverCard {
   children: ReactNode;
@@ -15,18 +16,36 @@ export function SentenceReferenceHoverCard({
   children,
   referenceData,
 }: SentenceReferenceHoverCard) {
+  const queryClient = useQueryClient();
   const { textDir } = useSearch({ strict: false });
-  const { data: page } = useQuery(
-    query.page(referenceData.bookId, referenceData.pageNumber)
+  const [pageData, setPageData] = useState<Page>();
+
+  const queryOptions = query.page(
+    referenceData.bookId,
+    referenceData.pageNumber
   );
+
+  async function handleFetchPageData() {
+    setPageData(
+      await queryClient.fetchQuery({
+        ...queryOptions,
+        queryKey: [
+          ...queryOptions.queryKey,
+          "sentenceId",
+          referenceData.sentenceId,
+        ],
+      })
+    );
+  }
+
   return (
-    <HoverCard width={280} shadow="md">
+    <HoverCard width={280} shadow="md" onOpen={handleFetchPageData}>
       <HoverCard.Target>{children}</HoverCard.Target>
-      <HoverCard.Dropdown w={600} hidden={!page}>
-        {page && textDir && (
+      <HoverCard.Dropdown w={600} hidden={!pageData}>
+        {pageData && textDir && (
           <ScrollAreaAutosize mah={300} type="hover">
             <SentenceTextPreview
-              paragraphs={page.paragraphs}
+              paragraphs={pageData.paragraphs}
               sentenceId={referenceData.sentenceId}
               textDir={textDir}
             />
